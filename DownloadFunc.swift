0b7c3ce888a46223815a11dd4ff3aee9faf6b9d2
Vu Anh Duc <vuanhduc2685@gmail.com>
2020-04-22 22:22:55 +0700

    Create DownloadFunc.swift

---
+++ DownloadFunc.swift
@@ -0,0 +1,8 @@
+import Foundation
+import FoundationNetworking
+func download(from sourceString: String, to destinationString: String) {
+    let source = URL(string: sourceString)!
+    let destination = URL(fileURLWithPath: destinationString)
+    let data = try! Data.init(contentsOf: source)
+    try! data.write(to: destination)
+}
